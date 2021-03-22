54f803e85fc7fe380e423836649f088abcec5b84
raajakutti <72712546+raajakutti@users.noreply.github.com>
2021-03-21 23:41:04 -0500

    Added Files

--- commands.sh
+++ commands.sh
@@ -1,1 +1,11 @@
-docker run --name taskmeds-redis -d dvzpoc/redis:latest redis-server --appendonly yes
+Jenkins
+-------
+PWD: 35a47472396b4e2fb48df5d19fc8a1f6
+
+taskmeui
+---------
+docker run -d -p 5000:5000 --name taskmeui dvzpoc/taskmeui:latest 
+
+taskmedb
+--------
+docker run -d -p 3000:3000 --name taskmedb dvzpoc/taskmedb:latest 
